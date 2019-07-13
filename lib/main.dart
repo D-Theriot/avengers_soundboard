d4a144908926c38bd4bf09b77437a5b32962a182
D-Theriot <dylantheriot@gmail.com>
2019-07-12 21:47:12 -0500

    initial flutter project creation

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(MaterialApp(
+  title: 'Avenger\'s Soundboard',
+  //debugShowCheckedModeBanner: false,
+  home: Home(),
+));
+
